3ff99af33fc94bffe31021eb567cf3824fdff596
jonatan-perez <jonatanp10@gmail.com>
2021-12-06 21:40:58 -0800

    Add day 7 problems and solutions

---
+++ day7/problem2_solution.py
@@ -0,0 +1,17 @@
+with open("input.txt") as f:
+    crab_positions = [int(x) for x in f.readline().rstrip('\n').split(",")]
+
+mode_pos = max(set(crab_positions), key=crab_positions.count)
+mean_pos = round((sum(crab_positions) / len(crab_positions)))
+
+fuel_spent = sum([(((pos * pos) + pos) / 2) for pos in crab_positions])
+
+for i in range(min(mean_pos, mode_pos), max(mean_pos, mode_pos) + 1): 
+    curr_fuel_spent = 0
+    for pos in crab_positions:
+        dist = abs((pos - i))
+        curr_fuel_spent += (((dist * dist) + dist) / 2)
+    if curr_fuel_spent < fuel_spent:
+        fuel_spent = curr_fuel_spent
+
+print(fuel_spent)
