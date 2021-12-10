ac09b4e188a5e5169b64d10e21adb62363202fdf
jonatan-perez <jonatanp10@gmail.com>
2021-12-09 22:09:40 -0800

    Add day 10 problems and solutions

---
+++ day10/problem1_solution.py
@@ -0,0 +1,26 @@
+open_chunks = "([{<"
+corrupted = []
+stack = []
+
+with open("input.txt") as f:
+    for line in f:
+        for char in line:
+            if char in open_chunks:
+                stack.append(char)
+            else: 
+                last_char = stack.pop()
+                if (char == ")" and last_char != "(") or (char == "]" and last_char != "[") or (char == "}" and last_char != "{") or (char == ">" and last_char != "<"):
+                    corrupted.append(char)
+                    break
+
+total = 0
+for corr in corrupted: 
+    if corr == ")":
+        total += 3
+    elif corr == "]":
+        total += 57
+    elif corr == "}":
+        total += 1197
+    elif corr == ">":
+        total += 25137
+print(total)
