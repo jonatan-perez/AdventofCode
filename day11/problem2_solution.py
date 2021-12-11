84806a754c0ff25ae352bb8fb3b6dfd6a78a46bf
jonatan-perez <jonatanp10@gmail.com>
2021-12-11 09:42:58 -0800

    Add day 11 problems and solutions

---
+++ day11/problem2_solution.py
@@ -0,0 +1,52 @@
+with open("input.txt") as f:
+    levels = [line.replace("\n", "") for line in f]
+    octopi = []
+    for level in levels: 
+        row = []
+        for energy in level:
+            row.append(int(energy))
+        octopi.append(row)
+
+flashes = 0
+
+def process_step():
+    global octopi
+    for x in range(10):
+        for y in range(10):
+            octopi[x][y] += 1
+
+    for x in range(10):
+        for y in range(10):
+            if octopi[x][y] > 9:
+                flash(x, y)
+
+    for x in range(10):
+        for y in range(10):
+            if octopi[x][y] == -1:
+                octopi[x][y] = 0
+
+def flash(x, y):
+    global octopi
+    global flashes
+
+    flashes += 1
+    octopi[x][y] = -1
+    for row in [-1, 0, 1]:
+        for col in [-1, 0, 1]:
+            new_x = x + row
+            new_y = y + col
+            if 0 <= new_x < 10 and 0 <= new_y < 10 and octopi[new_x][new_y] != -1:
+                octopi[new_x][new_y] += 1
+                if octopi[new_x][new_y] >= 10:
+                    flash(new_x, new_y)
+
+done = False
+steps = 0
+while not done:
+    process_step()
+    steps += 1
+    flashes = sum([sum(line) for line in octopi])
+    if flashes == 0:
+        done = True
+
+print(steps)
