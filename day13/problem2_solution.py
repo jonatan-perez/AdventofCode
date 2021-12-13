ff0cb06180e840ed176b7ec682fcec89b4638346
jonatan-perez <jonatanp10@gmail.com>
2021-12-13 13:28:30 -0800

    Add day13 problem and solution

---
+++ day13/problem2_solution.py
@@ -0,0 +1,64 @@
+with open("input.txt") as f:
+    marks = [line.replace("\n", "") for line in f]
+    for i in range(len(marks)):
+        if marks[i] == "":
+            dot_strings = marks[:i]
+            instructions = marks[i + 1: len(marks)]
+dots = []
+#keep track of max x,y values to get size of grid
+curr_max_x = 0
+curr_max_y = 0
+for dot in dot_strings:
+    dot_split = dot.split(",")
+    x = int(dot_split[0])
+    y = int(dot_split[1])
+    dots.append([x, y])
+    if x > curr_max_x:
+        curr_max_x = x
+    if y > curr_max_y:
+        curr_max_y = y
+
+curr_max_x += 1
+curr_max_y += 1
+grid = [["."] * curr_max_x for i in range(curr_max_y)]
+
+for dot in dots:
+    x = dot[0]
+    y = dot[1]
+    grid[y][x] = "#" #lists stack on each other so you first have to get y then x
+
+for instruction in instructions:
+    axis = instruction[11]
+    line = int(instruction[13:])
+    if axis == "y":
+        temp = grid
+        grid = temp[:line]
+        discarded_grid = temp[line + 1:]
+        reversed_disc_grid = discarded_grid[::-1]
+        for y in range(len(reversed_disc_grid)): #reversed because you are folding
+            for x in range(len(reversed_disc_grid[y])):
+                if reversed_disc_grid[y][x] == "#":
+                    grid[y][x] = "#"
+
+    elif axis == "x":
+        temp = grid
+        grid = []
+        discarded_grid = []
+        reversed_disc_grid = []
+        for row in temp:
+            grid.append(row[:line])
+            discarded_grid.append(row[line + 1:])
+
+        for row in discarded_grid:
+            reversed_disc_grid.append(row[::-1])
+
+        for y in range(len(reversed_disc_grid)): #reversed because you are folding
+            for x in range(len(reversed_disc_grid[y])):
+                if reversed_disc_grid[y][x] == "#":
+                    grid[y][x] = "#"
+
+for row in grid:
+    string_row = ""
+    for val in row:
+        string_row += val
+    print(string_row)
