1d9d4e6b17fda3bf29120ea04640f3f0acd97ab7
jonatan-perez <jonatanp10@gmail.com>
2021-12-14 08:15:28 -0800

    Add day 6 problem and solution

---
+++ day06/problem1_solution.py
@@ -0,0 +1,17 @@
+with open("input.txt") as f:
+   lanternfish =  [int(i) for i in f.readline().strip("\n").split(",")]
+
+
+def processOneDay(lanternfish, days):
+    new_fish = []
+    if days == 0:
+        return len(lanternfish)
+    for fish in lanternfish:
+        if fish == 0:
+            new_fish.append(8)
+            new_fish.append(6)
+        else: 
+            new_fish.append(fish - 1)
+    return processOneDay(new_fish, days - 1)
+
+print(processOneDay(lanternfish, 80))
---
+++ day06/problem2_solution.py
@@ -0,0 +1,23 @@
+with open("input.txt") as f:
+    lanternfish = [0 for i in range(9)]
+    fish_list =  [int(i) for i in f.readline().strip("\n").split(",")]
+    for fish in fish_list:
+        lanternfish[fish] += 1
+
+def processOneDay(lanternfish, days_left):
+    new_lanternfish = [0 for i in range(9)]
+    if days_left == 0:
+        return sum(lanternfish)
+    else:
+        for day in range(9):
+            curr_fish = lanternfish[day]
+            if day == 0:
+                new_lanternfish[6] += curr_fish
+                new_lanternfish[8] += curr_fish
+                new_lanternfish[0] = 0
+            else: 
+                new_lanternfish[day - 1] += curr_fish
+
+        return processOneDay(new_lanternfish, days_left - 1)
+
+print(processOneDay(lanternfish, 256))
