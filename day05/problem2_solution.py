b806c16395634c4c2f3c7bf9e67a77bd01e3d515
jonatan-perez <jonatanp10@gmail.com>
2021-12-13 19:13:20 -0800

    Add day 5 problem and solutions

---
+++ day05/problem2_solution.py
@@ -0,0 +1,71 @@
+from collections import Counter
+
+with open("input.txt") as f:
+    instructions = []
+    for line in f:
+        split_line = line.split(" -> ")
+        start = [int(num) for num in split_line[0].split(",")]
+        end = [int(num) for num in split_line[1].split(",")]
+        instructions.append([start, end])
+
+vent_lines = Counter()
+
+for line in instructions:
+    x1 = line[0][0]
+    y1 = line[0][1]
+    x2 = line[1][0]
+    y2 = line[1][1]
+
+    if x1 == x2:
+        if y1 < y2:
+            start = y1
+            end = y2
+        else: 
+            start = y2
+            end = y1
+        while start <= end:
+            vent_lines[x1, start] += 1
+            start +=1
+    elif y1 == y2:
+        if x1 < x2:
+            start = x1
+            end = x2
+        else:
+            start = x2
+            end = x1
+        while start <= end:
+            vent_lines[start, y1] += 1
+            start += 1
+    elif x1 < x2:
+        start = x1
+        end = x2
+        if y1 < y2:
+            while start <= end:
+                vent_lines[start, y1] += 1
+                start += 1
+                y1 += 1
+        else:
+            while start <= end:
+                vent_lines[start, y1] += 1
+                start += 1
+                y1 -= 1
+    elif x2 < x1:
+        start = x2
+        end = x1
+        if y2 < y1:
+            while start <= end:
+                vent_lines[start, y2] += 1
+                start += 1
+                y2 += 1
+        else:
+            while start <= end:
+                vent_lines[start, y2] += 1
+                start += 1
+                y2 -= 1
+
+total_overlap = 0
+for key in vent_lines.keys():
+    if vent_lines[key] > 1:
+        total_overlap += 1
+
+print(total_overlap)
