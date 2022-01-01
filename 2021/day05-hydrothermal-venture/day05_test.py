cc1f244f09322c1622db50326a6db67f7b28380c
jonatan-perez <jonatanp10@gmail.com>
2022-01-01 00:22:42 -0800

    Refactor code into functions and add testing to day05

---
+++ 2021/day05-hydrothermal-venture/day05_test.py
@@ -0,0 +1,17 @@
+import pytest
+import problem1
+import problem2
+
+with open("2021/day05-hydrothermal-venture/test_input.txt") as f:
+    instructions = []
+    for line in f:
+        split_line = line.split(" -> ")
+        start = [int(num) for num in split_line[0].split(",")]
+        end = [int(num) for num in split_line[1].split(",")]
+        instructions.append([start, end])
+
+def test_problem1():
+    assert problem1.calc_overlaps(instructions) == 5
+
+def test_problem2():
+    assert problem2.calc_overlap(instructions) == 12
