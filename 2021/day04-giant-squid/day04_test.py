6fb4a69f92cb055418e7b4350cf73238454ca6ee
jonatan-perez <jonatanp10@gmail.com>
2022-01-01 20:32:59 -0800

    Fix typo incorrect import in day04_test.py

--- 2021/day04-giant-squid/day04_test.py
+++ 2021/day04-giant-squid/day04_test.py
@@ -1,14 +1,14 @@
 import pytest
-import problem1
-import problem2
+import day04_problem1
+import day04_problem2
 
 with open("2021/day04-giant-squid/test_input.txt") as f:
     input = [line.replace("\n", "") for line in f]
     numbers_drawn = [int(i) for i in input[0].split(",")]
 
-boards = problem1.makeBoards(input)
+boards = day04_problem1.makeBoards(input)
 def test_problem1():
-    assert problem1.playBingo(numbers_drawn, boards) == 4512
+    assert day04_problem1.playBingo(numbers_drawn, boards) == 4512
 
 def test_problem2():
-    assert problem2.playBingo(numbers_drawn, boards) == 1924
+    assert day04_problem2.playBingo(numbers_drawn, boards) == 1924
